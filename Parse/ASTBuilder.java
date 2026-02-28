a1738402d4c196366c3e4c7c0bdc3f976c926be4
Brayden Robertson <145622684+RaydenBobertson@users.noreply.github.com>
2026-02-28 12:35:07 -0600

    Implemented Declarations + Added visitType Function
    Completed Part 2 and Implemented VisitType

--- Parse/ASTBuilder.java
+++ Parse/ASTBuilder.java
@@ -37,15 +37,95 @@ public class ASTBuilder extends gParserBaseVisitor<Absyn> {
       return decls;
    }
 
+@Override
+public Absyn visitType(gParser.TypeContext ctx) {
+
+    boolean isConst = ctx.CONST() != null;
+    String name = ctx.type_name().getText();
+    int ptrs = ctx.STAR().size();
+
+    DeclList brackets;
+    if (ctx.brackets_list() != null) {
+        brackets = (DeclList) visit(ctx.brackets_list());
+    } else {
+        brackets = new DeclList(0);
+    }
+
+    return new Type(0, isConst, name, ptrs, brackets);
+}
+
+//Part 2
+
    @Override
-   public Absyn visit__(gParser.__Context ctx) {
-      return IfStmt(
-            0,
-            visit(ctx.expr()),
-            visit(ctx.statement()),
-            new EmptyStmt(0)
-      );
+   public Absyn visitVarDecl(gParser.VarDeclContext ctx){
+      Type type = (Type) visit(ctx.type());
+      String name = ctx.ID().getText();
+
+      Exp init;
+
+      if (ctx.initialization() == null) {
+           init = new EmptyExp(0);
+      } else {
+           init = (Exp) visit(ctx.initialization().initializer());
+      }
+
+      return new VarDecl(0, type, name, init);
    }
 
+    @Override
+    public Absyn visitFunDecl(gParser.FunDeclContext ctx) {
+
+        Type retType = (Type) visit(ctx.type());
+        String name = ctx.ID().getText();
+
+        DeclList params;
+        if (ctx.parameters() != null) {
+            params = (DeclList) visit(ctx.parameters());
+        } else {
+            params = new DeclList(0);
+        }
+
+        Stmt body = null;
+        if (ctx.statement() != null) {
+            body = (Stmt) visit(ctx.statement());
+        }
+        return new FunDecl(0, retType, name, params, body);
+    }
+
+    @Override
+    public Absyn visitTypedefDecl(gParser.TypedefDeclContext ctx) {
+        Type type = (Type) visit(ctx.type());
+        String name = ctx.ID().getText();
+        return new Typedef(0, type, name);
+    }
+
+    @Override
+    public Absyn visitStructOrUnionDecl(gParser.StructOrUnionDeclContext ctx) {
+        String name = ctx.ID(0).getText();
+
+        DeclList members = new DeclList(0);
+        for (int i = 0; i < ctx.type().size(); i++) {
+            Type t = (Type) visit(ctx.type(i));
+            String field = ctx.ID(i + 1).getText();
+            members.list.add(new StructMember(0, t, field));
+        }
+
+        if (ctx.STRUCT() != null) {
+            return new StructDecl(0, name, members);
+        } else {
+            return new UnionDecl(0, name, members);
+        }
+    }
+
+    @Override
+    public Absyn visitParameters(gParser.ParametersContext ctx) {
+        DeclList params = new DeclList(0);
+        for (int i = 0; i < ctx.type().size(); i++) {
+            Type t = (Type) visit(ctx.type(i));
+            String name = ctx.ID(i).getText();
+            params.list.add(new Parameter(0, t, name));
+        }
+        return params;
+    }
 }
 
